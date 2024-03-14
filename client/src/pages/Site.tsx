14eed9aafd04f8c96f8075bb955edab19319c740
Sukhendu Sekhar Guria <sukhenduguria2017@gmail.com>
2024-03-14 12:05:56 +0530

    Update

--- client/src/pages/Site.tsx
+++ client/src/pages/Site.tsx
@@ -1,5 +1,5 @@
 import { useParams } from "react-router-dom";
-import axios, { AxiosError } from "axios";
+import axios from "axios";
 import { useEffect, useState } from "react";
 import { Line } from "react-chartjs-2";
 import { Chart, registerables } from "chart.js";
